a5eb4e9ab575ee2fe69bd0e6a9ecc0792afc3448
Pavana Teja T <teja1221@gmail.com>
2013-07-18 17:28:24 +0530

    First Commit

---
+++ Gemfile
@@ -0,0 +1,19 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.2.11'
+
+# Asset template engines
+gem 'haml'
+gem 'less'
+gem 'jquery-rails'
+gem 'therubyracer'
+
+gem 'sass-rails', '~> 3.2'
+gem 'bootstrap-sass', '~> 2.3.2.0'
+gem 'coffee-rails', '~> 3.2.1'
+gem 'uglifier', '>= 1.0.3'
+
+group :test do
+  # Pretty printed test output
+  gem 'turn', :require => false
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Activesphere::Application.config.secret_token = '0c481e6caf68dda49b8f769315b0a86182c238f36b6ec741e85b1023212398735b1cd8ec532413e4691459243b339c9c3dd2220e7d9ab93c2921ef956b66c83c'
